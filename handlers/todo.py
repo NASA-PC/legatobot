e850b0de1cf30f983cc9644ecf0b1af5eaf03538
N/A <Thorndrop@users.noreply.github.com>
2016-01-15 14:28:46 +0200

    Update todo.py

--- handlers/todo.py
+++ handlers/todo.py
@@ -6,10 +6,10 @@ class Handler:
         self.brain = brain; # Vrain is not used in this example, but it is usefull if you want i.e the name of the bot
 
     def canHandle(self, msg):
-        return msg.command == 'PRIVMSG' and msg.msg == '#todo';
+        return msg.command == "PRIVMSG" and msg.msg == "#todo";
 
     def handle(self, msg, resp):
-        resp.send('Just do it, fam!', msg.re())
+        resp.send("Just do it, fam!", msg.re())
         #for line in urllib.urlopen("https://raw.githubusercontent.com/Thorndrop/legatobot/master/todo.txt"): # Alternative way
         for line in open("todo.txt"):
             resp.send(line, msg.re())
