781e00e3740c836a5e33fa04a2a9a8b99539e846
N/A <Thorndrop@users.noreply.github.com>
2016-01-15 14:30:51 +0200

    Update quit.py

--- handlers/quit.py
+++ handlers/quit.py
@@ -6,7 +6,7 @@ class Handler:
         self.brain = brain; # Brain is not used in this example, but it is usefull if you want i.e the name of the bot
 
     def canHandle(self, msg):
-        return msg.command == 'PRIVMSG' and msg.msg.lower() == ('gtfo ' + self.brain.botnick).lower();
+        return msg.command == "PRIVMSG" and msg.msg.lower() == ("gtfo " + self.brain.botnick).lower();
 
     def handle(self, msg, resp):
-        resp.sendCommand('QUIT :rude tbh')
+        resp.sendCommand("QUIT :rude tbh")
