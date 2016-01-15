8c82d741975c9456038a53c22b29b3db04f28f96
N/A <Thorndrop@users.noreply.github.com>
2016-01-15 14:36:04 +0200

    Update LegatoBot.py

--- LegatoBot.py
+++ LegatoBot.py
@@ -3,18 +3,18 @@ import main
 
 brain = main.BrainsOfBot()
 
-modules = os.listdir('handlers');
+modules = os.listdir("handlers");
 modules.sort();
 for file in modules:
-    if(file.endswith('.py')):
+    if(file.endswith(".py")):
         file = file[:-3] # Remove .py
 
-        module = __import__('handlers.' + file).__getattribute__(file);
-        if('Handler' in dir(module)):
+        module = __import__("handlers." + file).__getattribute__(file);
+        if("Handler" in dir(module)):
             brain.registerHandler(module.Handler(brain));
-            print ('module "{0}" registered'.format(file));
+            print ("module "{0}" registered".format(file));
         else:
-            print ('Handler "{0}" does not have class Handler in it. Ignored.'.format(file));
+            print ("Handler "{0}" does not have class Handler in it. Ignored.".format(file));
 
 
 brain.start();
