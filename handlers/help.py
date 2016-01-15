f350118532d9d6376e11eae14a33a997eaf54e63
N/A <Thorndrop@users.noreply.github.com>
2016-01-15 14:33:04 +0200

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -9,7 +9,7 @@ class Handler:
         self.brain = brain; # Brain is not used in this example, but it is usefull if you want i.e the name of the bot
 
     def canHandle(self, msg):
-        return msg.command == 'PRIVMSG' and msg.msg == '#help';
+        return msg.command == "PRIVMSG" and msg.msg == "#help";
 
     def handle(self, msg, resp):
-        resp.send('#XdY to roll Y-sided dice x times, #stats to generate stats #clearstats to clear stats, #4chan for latest /balt/ thread, #8chan for the /balt/ board, #todo to see the To-Do list.', msg.re());
+        resp.send("#XdY to roll Y-sided dice x times, #stats to generate stats #clearstats to clear stats, #4chan for latest /balt/ thread, #8chan for the /balt/ board, #todo to see the To-Do list.', msg.re());
