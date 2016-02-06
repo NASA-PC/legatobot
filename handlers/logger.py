c2bd925b1c4241258b68122b9011878bbe288e61
piktaszuikis <buzisbuzulis@yahoo.com>
2016-02-06 20:42:23 +0200

    Implemented logger

---
+++ handlers/logger.py
@@ -0,0 +1,22 @@
+''' Logs everything to console '''
+
+ignored_commands = ['001', '002', '003', '004', '005', '251', '252',
+                    '254', '255', '265', '266', '250', '375', '372',
+                    '396', '332', '333', '366', '376'];
+
+class Handler:
+    
+    def __init__(self, brain):
+        self.brain = brain;
+    
+    def canHandle(self, msg):
+        
+        if(msg.command == 'PRIVMSG'):
+            print('{0:12}: {1}'.format(msg.user, msg.msg));
+        elif (not msg.command in ignored_commands):
+            print('--- {0}/{1} --- {2}'.format(msg.command, msg.user, msg.msg));
+
+        return False;
+        
+    def handle(self, msg, resp):
+        pass;
